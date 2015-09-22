71e0325673953fac67458dd685ae279e84f46fe0
Mike Przybylski <mike.przybylski@appdynamics.com>
2015-09-22 15:24:57 -0700

    Assassin typo fix and IO redirection changes
    Changed "this script must be run on the passive node" to
    "this script must be run on the active node"
    
    Updated sql() so that STDERR redirection happens before the pipe.
    ( Putting it after the pipe allowed mysql client error messages
    to be spewed on the console )
    
    Silenced standard output from tee by default.  Can easily be made noisy again
    by commenting out silence=">/dev/null"
    
    Change-Id: I2ac8d5d19ffa0e19772cffeefed6758f47837755

--- linux/assassin.sh
+++ linux/assassin.sh
@@ -9,6 +9,9 @@
 # problems, since replication is off
 # 
 
+# comment out to print logging messages to STDOUT
+silence=">/dev/null"
+
 #
 # this may need editing to conform with your controller install
 #
@@ -57,7 +60,7 @@ function remservice {
 
 function sql {
 	echo "$2 | $MYSQL --host=$1 $CONNECT controller" | $PWBLOCK >> $as_log
-	echo "$2" | $MYSQL --host=$1 $CONNECT controller | tee -a $as_log 2>> $as_log
+	echo "$2" | $MYSQL --host=$1 $CONNECT controller 2>> $as_log | tee -a $as_log
 }
 
 echo "  -- assassin log" `date` >> $as_log
@@ -67,15 +70,15 @@ echo "  -- assassin log" `date` >> $as_log
 # exit with a non-zero status and complain.
 #
 if [ ! -d "$APPD_ROOT" ] ; then
-	echo controller root $APPD_ROOT is not a directory | tee -a $as_log
+	echo controller root $APPD_ROOT is not a directory | tee -a $as_log $silence
 	exit 1
 fi
 if [ ! -w "$APPD_ROOT/db/db.cnf" ] ; then
-	echo db configuration $APPD_ROOT/db is not a directory | tee -a $as_log
+	echo db configuration $APPD_ROOT/db is not a directory | tee -a $as_log $silence
 	exit 1
 fi
 if [ ! -x "$MYSQL" ] ; then
-	echo controller root $MYSQL is not executable | tee -a $as_log
+	echo controller root $MYSQL is not executable | tee -a $as_log $silence
 	exit 1
 fi
 
@@ -86,7 +89,7 @@ mode=`sql localhost \
  "select * from global_configuration_local where name = 'appserver.mode'\G" |
  awk '/value:/ { print $2}'`
 if [ "$mode" == "passive" ] ; then
-	echo "this script must be run on the passive node" | tee -a $as_log
+	echo "this script must be run on the active node" | tee -a $as_log $silence
 	exit 1
 fi
 
@@ -97,7 +100,7 @@ type=`sql localhost \
  "select * from global_configuration_local where name = 'ha.controller.type'\G" |
  awk '/value:/ { print $2}'`
 if [ "$type" == "primary" ] ; then
-	echo "assassin unneeded" | tee -a $as_log
+	echo "assassin unneeded" | tee -a $as_log $silence
 	exit 1
 fi
 
@@ -106,7 +109,7 @@ fi
 # skip-slave start is set in out db.cnf
 # the slave is not running
 if ! grep -q "^skip-slave-start=true" $DBCNF ; then
-	echo slave not disabled | tee -a $as_log
+	echo slave not disabled | tee -a $as_log $silence
 	exit 1
 fi
 primary=unset
@@ -117,15 +120,15 @@ eval `sql localhost "show slave status\G" | awk '
     /Master_Host:/ {print "primary=",$2}
 '`
 if [ "$slave_sql" != "No" ] ; then
-	echo slave SQL running | tee -a $as_log
+	echo slave SQL running | tee -a $as_log $silence
 	exit 1
 fi
 if [ "$slave_io" != "No" ] ; then
-	echo slave IO running | tee -a $as_log
+	echo slave IO running | tee -a $as_log $silence
 	exit 1
 fi
 if [ "$primary" == "unset" ] ; then
-	echo "replication not set up - primary unset" | tee -a $as_log
+	echo "replication not set up - primary unset" | tee -a $as_log $silence
 	exit 1
 fi
 	
@@ -135,7 +138,7 @@ fi
 # so it stays down.
 #
 
-echo "assassin committed" | tee -a $as_log
+echo "assassin committed" | tee -a $as_log $silence
 
 echo $$ >$ASSASSIN
 
@@ -152,7 +155,7 @@ while true ; do
 	type=`sql localhost \
 		 "select * from global_configuration_local where name = 'ha.controller.type'\G" | awk '/value:/ { print $2}'`
 	if [ "$type" == "primary" ] ; then
-		echo "assassin disabled" | tee -a $as_log
+		echo "assassin disabled" | tee -a $as_log $silence
 		exit 1
 	fi
 
@@ -170,7 +173,7 @@ while true ; do
 		wq
 	DISABLE
 	if ! ssh $primary grep -q skip-slave-start $DBCNF ; then
-		echo "skip-slave-start insert failed" | tee -a $as_log
+		echo "skip-slave-start insert failed" | tee -a $as_log $silence
 		continue;
 	fi
 
@@ -181,7 +184,7 @@ while true ; do
 
 	# check if this happened
 	if ! sql $primary "select value from global_configuration_local where name = 'appserver.mode';" | grep -q passive ; then
-		echo "set passive failed" | tee -a $as_log
+		echo "set passive failed" | tee -a $as_log $silence
 		continue;
 	fi
 
